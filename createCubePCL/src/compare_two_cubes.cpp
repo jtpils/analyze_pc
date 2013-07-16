e8a54bfe78523e79259537cad199f6f4c8c4f325
sksavant <sksavant@9e839a59-819f-469c-a56d-6f7bff314291>
2013-07-16 09:56:11 +0000

    coloring the cube : spawning two cubes
    git-svn-id: svn+ssh://panda.frc.ri.cmu.edu/projects/aeroscout/software/repository/trunk/ca_stacks/src/sandbox/analyze_pc@5086 9e839a59-819f-469c-a56d-6f7bff314291

---
+++ createCubePCL/src/compare_two_cubes.cpp
@@ -0,0 +1,17 @@
+#include <createCubePCL/pcl_cube.h>
+
+int main(int argc, char ** argv){
+    ros::init (argc, argv, "two_cubes_compare");
+    PCLCube* c1 = new PCLCube;
+    PCLCube* c2 = new PCLCube;
+    c1->colorIt(255,0,0);
+    c2->colorIt(0,255,0);
+    ros::Rate loop_rate(100);
+    while(ros::ok()){
+        c1->publishPointCloud();
+        c2->publishPointCloud();
+        ros::spinOnce(); //Not getting any callbacks but just in case :P
+        loop_rate.sleep();
+    }
+    return 0;
+}
