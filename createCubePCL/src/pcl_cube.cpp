d448dd5bdb4f59eb3949bea3e8bd549d50fa7b01
sksavant <sksavant@9e839a59-819f-469c-a56d-6f7bff314291>
2013-07-16 09:56:08 +0000

    changing pointnormal to point for cube center. Need two axes to totally define a cube
    git-svn-id: svn+ssh://panda.frc.ri.cmu.edu/projects/aeroscout/software/repository/trunk/ca_stacks/src/sandbox/analyze_pc@5079 9e839a59-819f-469c-a56d-6f7bff314291

--- createCubePCL/src/pcl_cube.cpp
+++ createCubePCL/src/pcl_cube.cpp
@@ -9,10 +9,9 @@ PCLCube::PCLCube(){
     cube_center.x = 0;
     cube_center.y = 0;
     cube_center.z = 0;
-    cube_center.normal[0] = 0;
-    cube_center.normal[1] = 0;
-    cube_center.normal[2] = 1;
-    //std::cerr << cube_center << "\n";
+    cube_axes[0] = pcl::Normal(0,0,1); //The Z-axis
+    cube_axes[1] = pcl::Normal(0,1,0); //The Y-axis
+    std::cerr << cube_axes[0] << cube_axes[1] << "\n";
 
     dense = false;
     scale = 1.0;
@@ -38,20 +37,27 @@ void PCLCube::generatePlanePoints(pcl::PointNormal center, float scale){
     }
 }
 
+void PCLCube::findFaceCenters(pcl::PointNormal &fc, pcl::Normal normal, bool direction){
+
+}
+
 void PCLCube::generatePoints(){
-    cube_cloud.header.frame_id = "cube_cloud";
     float dense_scale = scale;
+    pcl::PointNormal face_centers[6];
+    bool direction = true;
     if (dense){
         dense_scale*=DENSE_FACTOR;
     }
+    cube_cloud.header.frame_id = "cube_cloud";
     cube_cloud.height = POINTS_PER_UNIT*dense_scale;
     cube_cloud.width = 6*POINTS_PER_UNIT*dense_scale;
     cube_cloud.points.resize(cube_cloud.width*cube_cloud.height);
     //std::cerr << cube_cloud.width << " " << cube_cloud.height << "\n";
     //std::cerr << POINTS_PER_UNIT << " " << DENSE_FACTOR << " " << dense_scale << "\n";
-    pcl::PointNormal face_centers[6];
-    // TODO : get the correct face_centers with normals
     for (int i=0; i<6; ++i){
+        // TODO : get the correct face_centers with normals
+        findFaceCenters(face_centers[i],cube_axes[i/2],direction);
+        direction = (!direction);
         generatePlanePoints(face_centers[i], scale);
     }
 }
